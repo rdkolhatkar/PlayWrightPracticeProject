69f683ef05ba0c5e6a50b3928bae9e7cd33bc94b
Ratnakar <ratnakarkolhatkar@gmail.com>
2026-02-13 16:39:58 +0530

    Debugging

--- src/JavaScript/Web/bdd/Support/Hooks.js
+++ src/JavaScript/Web/bdd/Support/Hooks.js
@@ -2,11 +2,18 @@ import { Before, After } from '@cucumber/cucumber';
 import { chromium } from '@playwright/test';
 import { setDefaultTimeout } from '@cucumber/cucumber';
 // import { Before, After, setDefaultTimeout } from '@cucumber/cucumber';
+// npm install cross-env --save-dev
 
 setDefaultTimeout(60 * 1000);
 
 Before(async function () {
-  this.browser = await chromium.launch({ headless: false });
+  this.browser = await chromium.launch(
+    { 
+      headless: false,
+      slowMo: 500,
+      args: ['--auto-open-devtools-for-tabs'] // better than devtools:true 
+    }
+  );
   this.context = await this.browser.newContext();
   this.page = await this.context.newPage();
 });
